93c8db888d4e48d2561fcac3c2763786a4bed956
Somnath Mandal <116511360+somnath9211@users.noreply.github.com>
2023-08-27 22:07:27 +0530

    Add files via upload

---
+++ 6th Project Assignment 4 JavaScript Events/index.js
@@ -0,0 +1,28 @@
+var MainImg = document.getElementById('MainImg');
+var smallImg = document.getElementsByClassName('previewImg');
+
+smallImg[1].onclick = function () {
+        MainImg.src = smallImg[1].src;
+}
+
+smallImg[2].onclick = function () {
+    MainImg.src = smallImg[2].src;
+}
+
+smallImg[3].onclick = function () {
+    MainImg.src = smallImg[3].src;
+}
+
+smallImg[4].onclick = function () {
+    MainImg.src = smallImg[4].src;
+}
+
+smallImg[5].onclick = function () {
+    MainImg.src = smallImg[5].src;
+}
+smallImg[6].onclick = function () {
+    MainImg.src = smallImg[6].src;
+}
+
+
+
